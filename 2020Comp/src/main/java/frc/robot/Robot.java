11881f343cd512c3f350501930c402c803ed4fd6
jamiha <34746242+jamiha@users.noreply.github.com>
2020-01-09 18:48:25 -0500

    Added comments
    Comments to help people understand the code when I am not at practice as well it adds some more understanding to people who may want to reuse this code.

--- 2020Comp/src/main/java/frc/robot/Robot.java
+++ 2020Comp/src/main/java/frc/robot/Robot.java
@@ -10,21 +10,37 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;;
 
 public class Robot extends TimedRobot {
+  //Declaration of the drive 
   private DifferentialDrive Robot;
+
+  //Declaration of the XBOX controller
   private XboxController controller;
 
+  //Declaration of the Spark Max's
   private CANSparkMax left;
   private CANSparkMax right;
 
   public void robotInit() {
+
+    //Implementation of the Spark Max's
     left = new CANSparkMax(1, MotorType.kBrushless);
     right = new CANSparkMax(2, MotorType.kBrushless);
 
+    //Implementation of the drive
     Robot = new DifferentialDrive(left, right);
+
+    //Implementation of the Xbox Controller
     controller = new XboxController(0);
+    
   }
 
   public void teleopPeriodic() {
+    /*How the robot uses the joystick inputs
+    To change the speed, change or add a decimal number after the () * by doing something like this: Robot.tankDrive(controller.getRawAxis(0) * 0.7, controller.getRawAxis(1) * 0.7);
+    To invert the direction of your forward and backwards, do the same thing but make the decimal negative: Robot.tankDrive(controller.getRawAxis(0) * -0.7, controller.getRawAxis(1) * -0.7);
+    */
+
     Robot.tankDrive(controller.getRawAxis(0), controller.getRawAxis(1));
+
   }
 }
